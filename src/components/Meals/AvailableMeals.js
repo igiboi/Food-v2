365b24791d671ed8c1b1e6a7b22dad5a45c98555
Luigi Matias <luigi.matias@codehunter.com>
2022-09-29 11:26:22 -0400

    added a error inside a promise

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,10 +7,16 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
-     const response = await fetch('https://react-http26-default-rtdb.firebaseio.com/meals.json');
+     const response = await fetch('https://react-http26-default-rtdb.firebaseio.com/meals');
+
+     if (!response.ok) {
+      throw new Error('Something went wrong!');
+     }
+
      const responseData = await response.json();
 
      const loadedMeals = [];
@@ -28,13 +34,24 @@ const AvailableMeals = () => {
      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    })
   }, [])
 
   if (isLoading) {
     return(
       <section className={classes.mealsLoading}>
         <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }

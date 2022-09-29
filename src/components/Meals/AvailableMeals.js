c50469c90e79e15224bae0dbfe1fae747bb5230b
Luigi Matias <luigi.matias@codehunter.com>
2022-09-29 11:27:38 -0400

    added .json

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -11,7 +11,7 @@ const AvailableMeals = () => {
 
   useEffect(() => {
     const fetchMeals = async () => {
-     const response = await fetch('https://react-http26-default-rtdb.firebaseio.com/meals');
+     const response = await fetch('https://react-http26-default-rtdb.firebaseio.com/meals.json');
 
      if (!response.ok) {
       throw new Error('Something went wrong!');
